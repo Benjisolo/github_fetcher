a0d4c0be24bc69df81ef03645afd878c51845035
BenjiSolo <36042643+Benjisolo@users.noreply.github.com>
2020-09-15 17:54:30 +0800

    Template text

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-$(document).ready(function () {
+ $(document).ready(function () {
     var user = "";
     var count = 0;
+
+    const client_id = "Iv1.01a8f535dd4ca2c8"
+    const client_secret = "bdb0cc9173140424596b65bd38dea4471e3bce3d"
 
     $("#form").submit(function (event) {
         event.preventDefault();
 
         var username = $("#username").val();
 
-        // searchUsers(username);
-        countFollowers(username)
+        searchUsers(username);
+        countFollowers("octocat")
     });
 
     function searchUsers(username) {
-        $.get("https://api.github.com/search/users?q=" + username + "+in:user&per_page=100",
+        $.get(`https://api.github.com/search/users?q=${username}+in:user&per_page=100`,
             function (data) {
                 // console.log(data);
                 data.items.forEach((item) => {
-                    var followers = countFollowers(item.login);
+                    var followers = 0
                     user = 
                     `<div class="card" style="width: 18rem"> 
                         <img src="${item.avatar_url}" class="card-img-top"/>
@@ -33,7 +36,8 @@ $(document).ready(function () {
     }
 
     function countFollowers(username) {
-        $.get("https://api.github.com/users/" + username + "/followers", function(data) {
+        $.get(`https://api.github.com/users/${username}/followers`, 
+        function(data) {
             // console.log(data)
             data.items.forEach((item) => {
                 $('#result').append(`<h5>${item.login} </h5>`)
